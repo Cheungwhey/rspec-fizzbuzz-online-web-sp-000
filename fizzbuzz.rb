df875cc69efa26305d993f00fe3c710c6ead3190
charlon tamayo <charlont@gmail.com>
2020-12-13 01:49:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,16 +2,16 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+    "Fizz"
       
   elsif int % 5 == 0
-    puts "Buzz"
+    "Buzz"
   
   elsif int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
 
   else int % 3 != 0 || int % 5 != 0
-    puts "nil"
+    "nil"
   
   end
 end
